4a33782a2e177bd7038f48324fd6b44fc44eb223
Johannes <substans@gmail.com>
2013-09-26 09:48:36 +0200

    Update EasyAnimate.cs

--- EasyAnimate.cs
+++ EasyAnimate.cs
@@ -19,115 +19,148 @@ public class EasyAnimate
 
 	public struct State<T> : IState {
 		public delegate void EASetter(T pValue);
-		public delegate T EAInterpolate(T pFrom, T pTo, float pQuota);
-		public T startValue;
-		public T endValue; 
-		public float StartTime{ get; set; }
-		public float duration; //length of animation in seconds
-		public Action onCompleted; //action done on complete, does not fire if dissmissed or cleared
-		public EAInterpolate interpolator; //function used to interpolate a value over time
-		public EASetter setter; //function used to apply the interpolated value    
-		/// <summary>
-		/// Update is called by the manager
-		/// </summary>
-		/// <param name="pCurrentTime">current time in seconds</param>
-		/// <returns>true if still animating, false if done</returns>
-		public bool Update( float pCurrentTime ){
-			if (StartTime > pCurrentTime) { //before the animation starts
-				return true;
-			}
-			if ((StartTime + duration) <= pCurrentTime) { //the animation has ended
-				return false;
-			}
-			else { //while animating
-				if (setter != null) {
-					setter(interpolator(startValue, endValue, (pCurrentTime - StartTime) / duration));
+
+			public delegate T EAInterpolate(T pFrom,T pTo,float pQuota);
+
+			public T startValue;
+			public T endValue;
+
+			public float StartTime{ get; set; }
+
+			public float duration;
+			//length of animation in seconds
+			public Action onCompleted;
+			//action done on complete, does not fire if dissmissed or cleared
+			public EAInterpolate interpolator;
+			//function used to interpolate a value over time
+			public EASetter setter;
+			//function used to apply the interpolated value    
+			/// <summary>
+			/// Update is called by the manager
+			/// </summary>
+			/// <param name="pCurrentTime">current time in seconds</param>
+			/// <returns>true if still animating, false if done</returns>
+			public bool Update(float pCurrentTime)
+			{
+				if (StartTime > pCurrentTime)
+				{ //before the animation starts
+					return true;
 				}
-				return true;
-			}
-		}
-		public void Complete() {
-			if (setter != null) { setter(endValue); }
-			if (onCompleted != null) {
-				onCompleted();
-			}
-		}
-		public bool Paused {
-			set;
-			get;
-		}
-	}
-
-	//static Dictionary<object, Dictionary<string, IState>> _animStates = new Dictionary<object, Dictionary<string, IState>>();
-	List<Channel> _animStates = new List<Channel>();
-	IState _soloState = null;
-
-	/// <summary>
-	/// Add a new animation state to the manager only if no animation with the same object and channel is not playing
-	/// </summary>
-
-	/// <param name="pChannel">a name token</param>
-	/// <param name="pState">an EasyAnimate.State object</param>
-	public void SetIfVacant(string pChannel, IState pState) {
-		var newChan = new Channel(){
-			name = pChannel,
-			state = pState
-		};
-		int channelIndex = _animStates.FindIndex(p => p.name == pChannel);
-		if (channelIndex == -1)
-		{
-			_animStates.Add(newChan);
-		}
-	}
-	/// <summary>
-	/// Add a new animation state to the manager
-	/// </summary>
-
-	/// <param name="pChannel">a name token</param>
-	/// <param name="pState">an EasyAnimate.State object</param>
-	public void Set(string pChannel, IState pState) {
-		var newChan = new Channel(){
-			name = pChannel,
-			state = pState
-		};
-		int channelIndex = _animStates.FindIndex(p => p.name == pChannel);
-		if (channelIndex == -1)
-		{
-			_animStates.Add(newChan);
-		}
-		else
-		{
-			_animStates[channelIndex] = newChan;
-		}
-	}
-
-	public T Get<T>(string pChannel) where T : IState {
-		int channelIndex = _animStates.FindIndex(p => p.name == pChannel);
-		if (channelIndex == -1)
-		{
-			return default(T);
-		}
-		else
-		{
-			var result = _animStates[channelIndex].state;
-			if (result is T) {
-				return (T)result;
-			}
-			else {
-				throw new InvalidCastException("could not cast object of type " + result.GetType().Name + " to " + typeof(T).Name);
+				if ((StartTime + duration) <= pCurrentTime)
+				{ //the animation has ended
+					return false;
+				}
+				else
+				{ //while animating
+					if (setter != null)
+					{
+						setter(interpolator(startValue, endValue, (pCurrentTime - StartTime) / duration));
+					}
+					return true;
+				}
 			}
 
-		}
-	}
+			public void Complete()
+			{
+				if (setter != null)
+				{
+					setter(endValue);
+				}
+				if (onCompleted != null)
+				{
+					onCompleted();
+				}
+			}
 
-	internal void Update( float pDeltaTime = float.MinValue ){
-		if (pDeltaTime == float.MinValue){
-			pDeltaTime = Time.deltaTime;
+			public bool Paused
+			{
+				set;
+				get;
+			}
 		}
+		//static Dictionary<object, Dictionary<string, IState>> _animStates = new Dictionary<object, Dictionary<string, IState>>();
+		List<Channel> _animStates = new List<Channel>();
+		IState _soloState = null;
+
+		/// <summary>
+		/// Add a new animation state to the manager only if no animation with the same object and channel is not playing
+		/// </summary>
+
+		/// <param name="pChannel">a name token</param>
+		/// <param name="pState">an EasyAnimate.State object</param>
+		public void SetIfVacant(string pChannel, IState pState)
+		{
+			var newChan = new Channel()
+			{
+				name = pChannel,
+				state = pState
+			};
+			int channelIndex = _animStates.FindIndex(p => p.name == pChannel);
+			if (channelIndex == -1)
+			{
+				_animStates.Add(newChan);
+			}
+		}
+
+		/// <summary>
+		/// Add a new animation state to the manager
+		/// </summary>
+
+		/// <param name="pChannel">a name token</param>
+		/// <param name="pState">an EasyAnimate.State object</param>
+		public void Set(string pChannel, IState pState)
+		{
+			var newChan = new Channel()
+			{
+				name = pChannel,
+				state = pState
+			};
+			int channelIndex = _animStates.FindIndex(p => p.name == pChannel);
+			if (channelIndex == -1)
+			{
+				_animStates.Add(newChan);
+			}
+			else
+			{
+				_animStates[channelIndex] = newChan;
+			}
+		}
+
+		public T Get<T>(string pChannel) where T : IState
+		{
+			int channelIndex = _animStates.FindIndex(p => p.name == pChannel);
+			if (channelIndex == -1)
+			{
+				return default(T);
+			}
+			else
+			{
+				var result = _animStates[channelIndex].state;
+				if (result is T)
+				{
+					return (T)result;
+				}
+				else
+				{
+					throw new InvalidCastException("could not cast object of type " + result.GetType().Name + " to " + typeof(T).Name);
+				}
+
+			}
+		}
+	bool firstUpdate = true;
+	float _lastUpdateTime = float.MinValue;
+	internal void Update()
+	{
+		if (firstUpdate)
+		{
+			_lastUpdateTime = Time.time;
+			firstUpdate = false;
+		}
+		float deltaTime = Time.time - _lastUpdateTime;
 		for( int i = _animStates.Count -1 ; i >= 0; i--){
 			var state = _animStates[i].state;
 			if (state.Paused) { 
-				state.StartTime += pDeltaTime;
+				state.StartTime += deltaTime;
 			}
 			if (!state.Update(Time.time)) {
 				_animStates.RemoveAt(i);
@@ -138,6 +171,7 @@ public class EasyAnimate
 				state.Complete(); //must be called after key is removed in case the complete event triggers a reuse of the key.
 			}
 		}
+		_lastUpdateTime = Time.time;
 	}
 
 	/// <summary>
@@ -178,7 +212,13 @@ public class EasyAnimate
 	}
 
 	public void UnpauseAll() {
+		Update();
 		_animStates.ForEach(p => p.state.Paused = false);
+	}
+
+	public void PauseAll()
+	{
+		_animStates.ForEach(p => p.state.Paused = true);
 	}
 
 	public void Solo(string pKey) {
@@ -194,10 +234,18 @@ public class EasyAnimate
 		}
 	}
 
+	public bool IsAnimating()
+	{
+		return _animStates.Count > 0;
+	}
+
 	internal void Clear() {
 		_animStates.Clear();
 	}
 	internal bool HasState( string pChannel) {
 		return _animStates.FindIndex(p => p.name == pChannel) != -1;
+	}
+	public EasyAnimate(){
+
 	}
 }
